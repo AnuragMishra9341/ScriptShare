ca0b77fd73ba84875f4925840ed5aae479240b80
AnuragMishra9341 <anurag102005@gmail.com>
2025-11-28 20:47:11 +0530

    omitted morgan

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(cors({
 }));
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 
 
 // --------------------------- routes ---------------------------
